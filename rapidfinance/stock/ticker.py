7a4695807a49a17d6d8fed362a396e6aa016cb12
Marco Latella <mrclatella@gmail.com>
2022-11-09 17:55:50 +0100

    Update ticker.py

--- rapidfinance/stock/ticker.py
+++ rapidfinance/stock/ticker.py
@@ -9,15 +9,20 @@ from datetime import datetime
 
 
 class Ticker:
-    def __init__(self, symbol, region="US"):
+    def __init__(self, symbol):
+        assert isinstance(symbol, str)
         self.symbol = symbol
-        self.region = region
         self.info = self.get_summary()
-        self.profile_info = Summary(self.info.summaryProfile)
-        self.sector = self.info.summaryProfile['sector']
-        self.shortName = self.info.price['shortName']
-        self.currency = self.info.price['currency']
-        self.currencySymbol = self.info.price['currencySymbol']
+        if hasattr(self.info, 'summaryProfile'):
+            self.profile_info = self.info.summaryProfile
+            if hasattr(self.profile_info, 'sector'):
+                self.sector = self.info.summaryProfile.sector
+        if hasattr(self.info.price, 'shortName'):
+            self.shortName = self.info.price.shortName
+        if hasattr(self.info.price, 'currency'):
+            self.currency = self.info.price.currency
+        if hasattr(self.info.price, 'currencySymbol'):
+            self.currencySymbol = self.info.price.currencySymbol
         self.history = None
 
     def plot(self, columns, width=10, height=7):
@@ -36,14 +41,30 @@ class Ticker:
         plt.show()
 
     def get_summary(self):
+        """
+        It takes the symbol of the stock, makes a request to Yahoo Finance, and returns a summary of the stock
+        :return: A Summary object
+        """
         get_req = config.yahoo_host + endpoints.summary_endpt + self.symbol
         modules = ','.join(endpoints.summary_modules)
         params = {'modules': modules}
         response = self.send_request(get_req, params)
         summary = to_dict(response.text)
+        if summary['quoteSummary']['result'] is None:
+            raise Exception(summary['quoteSummary']['error'])
         return Summary(summary['quoteSummary']['result'][0])
 
     def download_history(self, start, end, interval, pre_post=False, div_split=False):
+        """
+        The function takes in a start date, end date, interval, and two optional parameters (pre_post and div_split) and
+        returns a pandas dataframe of the historical data.
+
+        :param start: The start date of the history you want to download
+        :param end: The end date of the data you want to download
+        :param interval: 1m, 5m, 15m, 30m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo
+        :param pre_post: Include pre-market and post-market data, defaults to False (optional)
+        :param div_split: If True, the history will include dividend and split data, defaults to False (optional)
+        """
         # Possible inputs for &interval=: 1m, 5m, 15m, 30m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo
         start = int(datetime.strptime(start, '%Y/%m/%d').timestamp())
         end = int(datetime.strptime(end, '%Y/%m/%d').timestamp())
@@ -69,9 +90,27 @@ class Ticker:
 
         self.history = df
 
+    def compute_return(self, df, periods):
+        returns = {}
+        if isinstance(periods, list):
+            for period in periods:
+                returns[period] = self.__compute_return(df, period)
+            return returns
+        else:
+            return self.__compute_return(df, periods)
+
+    def __compute_return(self, df, period):
+        assert isinstance(period, str)
+        tmp = df.resample(period).agg({"open": "first",
+                                       "high": "max",
+                                       "low": "min",
+                                       "close": "last"})
+        tmp['pct_change'] = tmp.close.pct_change()
+        tmp.dropna(inplace=True)
+        return round(tmp.iloc[-1][-1] * 100, 2)
+
     def __repr__(self):
         return f"Name: {self.shortName}\n" \
-               f"Sector: {self.sector}\n" \
                f"Symbol: {self.symbol}\n" \
                f"Currency: {self.currency}"
 

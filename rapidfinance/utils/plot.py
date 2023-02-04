878adcc2d2c3df991cbeda99bf55bece6c620843
Marco Latella <mrclatella@gmail.com>
2023-02-04 12:35:57 +0100

    Implemented Candlestick plot

--- rapidfinance/utils/plot.py
+++ rapidfinance/utils/plot.py
@@ -1,6 +1,17 @@
 import plotly.express as px
+import plotly.graph_objects as go
+
 
 def plot_line(df, columns, **kwargs):
     assert isinstance(columns, list)
     fig = px.line(df[columns], **kwargs)
+    fig.show()
+
+
+def plot_candlestick(df, **kwargs):
+    fig = go.Figure(data=[go.Candlestick(x=df.index,
+                                         open=df['open'],
+                                         high=df['high'],
+                                         low=df['low'],
+                                         close=df['adjclose'])])
     fig.show()
